917347389188af0bdc0ce189c6a7c53dec01445a
Cucinotta Giuseppe <peppecuci@hotmail.com>
2022-07-18 11:22:32 +0200

    commit hotel 3

---
+++ src/main/java/com/projet_hotel/hotel/DAOs/ReservationDAO.java
@@ -0,0 +1,85 @@
+package com.projet_hotel.hotel.DAOs;
+
+import com.projet_hotel.hotel.entity.Reservation;
+import com.projet_hotel.hotel.exceptions.EntityAlreadyExistsException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.RollbackException;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.Optional;
+
+public class ReservationDAO {
+
+    private final EntityManager manager;
+
+
+    public ReservationDAO(EntityManager manager) {
+        this.manager = manager;
+    }
+
+    //getOne
+    public Optional<Reservation> getById(int id ){
+        return Optional.ofNullable( manager.find(Reservation.class, id) );
+    }
+
+    //getAll
+    public List<Reservation> getAll(){
+        TypedQuery<Reservation> query = manager.createQuery("SELECT r FROM Reservation  r", Reservation.class);
+        return query.getResultList();
+    }
+
+    //insertOne
+    public void insert( Reservation toInsert ){
+
+        try{
+            beginTransact();
+            manager.persist(toInsert);
+            commit();
+        }
+        catch (RollbackException ex){
+            throw new EntityAlreadyExistsException(toInsert.getIdReservation(), Reservation.class);
+        }
+
+    }
+
+    //deleting
+    public void deleteById(int id){
+
+        beginTransact();
+        getById(id).ifPresent( manager::remove );
+        commit();
+
+    }
+
+
+    //updating
+    public void update( Reservation reservation ){
+
+        if( reservation == null )
+            throw new IllegalArgumentException("reservation cannot be null");
+
+        if( !existsById(reservation.getIdReservation()))
+            throw new EntityNotFoundException("Entity not found");
+
+        beginTransact();
+        manager.merge(reservation);
+        commit();
+    }
+
+    public boolean existsById(int id){
+        TypedQuery<Integer> query = manager.createQuery("SELECT COUNT(r) FROM Reservation r WHERE r.id = " + id, Integer.class);
+        return query.getSingleResult() > 0;
+    }
+
+    private void beginTransact(){
+        manager.getTransaction().begin();
+    }
+
+    private void commit(){
+        manager.getTransaction().commit();
+    }
+
+
+}
---
+++ src/main/java/com/projet_hotel/hotel/services/ReservationService.java
@@ -0,0 +1,62 @@
+package com.projet_hotel.hotel.services;
+
+import com.projet_hotel.hotel.DAOs.ReservationDAO;
+import com.projet_hotel.hotel.entity.Reservation;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ReservationService {
+
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+    EntityManager em = emf.createEntityManager();
+    ReservationDAO dao =  new ReservationDAO(em);
+
+    // region SINGLETON
+    private static ReservationService instance;
+    public static ReservationService getInstance(){
+        System.out.println("recup de l'instance du service");
+        return instance == null ? instance = new ReservationService() : instance;
+    }
+    private ReservationService() {}
+    // endregion
+
+    private final List<Reservation> list = new ArrayList<>();
+
+    public List<Reservation> getAll(){
+        return new ArrayList<>(list);
+    }
+
+
+    //getOne
+    public Optional<Reservation> oneOfIt(int id){
+        return dao.getById(id);
+    }
+
+    //getAll
+    public List<Reservation> allOfIt() {
+        return dao.getAll();
+    }
+
+    //insertOne
+    public void insert(Reservation toInsert){
+         dao.insert(toInsert);
+    }
+
+    //deleting
+    public void deleteIt(int id) {
+        dao.deleteById(id);
+    }
+
+    //updating
+    public void updateIt (Reservation reservation) {
+        dao.update(reservation);
+    }
+
+
+}
